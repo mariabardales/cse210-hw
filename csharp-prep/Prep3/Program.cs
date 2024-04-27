12e00c9595882f1c99d357454924aa3c613626fa
mariabardales <mariabardales.ing@gmail.com>
2024-04-26 20:15:54 -0500

    Changes Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        int guessint;
+        int cont=0;
+        do
+        {
+            Console.WriteLine("What is your guess? ");
+            string nguess=Console.ReadLine();
+            guessint=int.Parse(nguess);
+            if (guessint>number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessint<number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            cont=cont+1;
+        } while (guessint!=number);
+        Console.WriteLine($"Number of attempts: {cont}");
     }
 }
